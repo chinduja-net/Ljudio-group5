58292a9966bd35a04b7a2d9bfa2c114b2a85b72d
purreB <jakob.bergstrom@live.se>
2021-09-07 15:23:34 +0200

    Made our own play/pause buttons as well as mute/unmute

--- Frontend/src/routes/PlayerPage.jsx
+++ Frontend/src/routes/PlayerPage.jsx
@@ -15,10 +15,26 @@ function playerPage() {
   }
 
   function playNewSong() {
-    // ABBA
+    // This will be changed depending on queue list
     let videoId = 'uHU48c-dtqk';
     player.loadVideoById(videoId);
     player.playVideo();
+  }
+
+  function pauseBtn() {
+    player.pauseVideo(2);
+  }
+
+  function playBtn() {
+    player.playVideo();
+  }
+
+  function muteBtn() {
+    player.mute();
+  }
+
+  function unmuteBtn() {
+    player.unMute();
   }
 
   return (
@@ -26,6 +42,10 @@ function playerPage() {
       <Player onLoad={onPlayerLoad} />
       <h1>I have never seen such an ugly page before, BUT it works!</h1>
       <button onClick={playNewSong}>ABBA SONGS</button>
+      <button onClick={pauseBtn}>Pause Button</button>
+      <button onClick={playBtn}>Play Button</button>
+      <button onClick={muteBtn}>Mute Button</button>
+      <button onClick={unmuteBtn}>Unmute Button</button>
     </div>
   );
 }
