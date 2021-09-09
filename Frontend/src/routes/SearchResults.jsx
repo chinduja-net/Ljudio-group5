96516b46c0656a6455921edca9bd163c552f29ed
paccao <16489683+paccao@users.noreply.github.com>
2021-09-09 11:26:00 +0200

    Added functionality to render out different types of searchresults in the DOM

--- Frontend/src/routes/SearchResults.jsx
+++ Frontend/src/routes/SearchResults.jsx
@@ -5,21 +5,77 @@ import SearchBar from "../components/SearchBar"
 function SearchResults() {
 	const { searchResults } = useContext(SearchContext)
 
+	function renderSong({
+		id,
+		type,
+		name,
+		thumbnail,
+		artist,
+		album,
+		duration,
+		videoId,
+	}) {
+		return (
+			<div key={id}>
+				<img
+					className="resultThumbnailSquare"
+					src={thumbnail}
+					alt={artist + "'s cover thumbnail"}
+				/>
+				<div>
+					<h4>{name}</h4>
+					<p>{artist}</p>
+				</div>
+				<button type="button" className="resultSettingsBtn">
+					:{/* Add an icon */}
+				</button>
+			</div>
+		)
+	}
+	function renderArtist({ id, type, name, thumbnail, browseId }) {
+		return (
+			<div key={id}>
+				<img
+					className="resultThumbnailArtist"
+					src={thumbnail}
+					alt={name + "'s cover thumbnail"}
+				/>
+				<h3>{name}</h3>
+			</div>
+		)
+	}
+	function renderAlbum({ id, type, name, thumbnail, artist, browseId }) {
+		return (
+			<div key={id}>
+				<img
+					className="resultThumbnailSquare"
+					src={thumbnail}
+					alt={artist + "'s cover thumbnail"}
+				/>
+				<div>
+					<h4>{name}</h4>
+					<p>{artist}</p>
+				</div>
+			</div>
+		)
+	}
+
 	return (
 		<div id="root">
 			<h5>SearchResults.jsx</h5>
 			<SearchBar />
 			<div>
-				{searchResults ? // When looping through the search results we can limit the loop with user input
-					searchResults.map(({id, videoId, type, name, artist, thumbnail}) => (
-						<div key={id}>
-							<p>{videoId}</p>
-							<p>{type}</p>
-							<h2>{name}</h2>
-							<h3>{artist}</h3>
-							<img src={thumbnail} alt={name + " thumbnail."} />
-						</div>
-					)) : console.warn("Didn't work, sorry")}
+				{searchResults // When looping through the search results we can limit the loop with user input
+					? searchResults.map((obj) => {
+							return obj.type === "song"
+								? renderSong(obj)
+								: obj.type === "artist"
+								? renderArtist(obj)
+								: obj.type === "album"
+								? renderAlbum(obj)
+								: null
+					  })
+					: null}
 			</div>
 		</div>
 	)
