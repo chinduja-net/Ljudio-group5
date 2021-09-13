15891ef70b0607784e3cbe24b84defe8fd1d2238
chinduja-net <cindhujavijayaraj@gmail.com>
2021-09-13 23:46:29 +0200

    name updated

--- Frontend/src/context/SongProvider.jsx
+++ Frontend/src/context/SongProvider.jsx
@@ -1,34 +1,38 @@
-import React, { createContext, useState } from "react"
+import React, { createContext, useState } from "react";
 
-export const SearchContext = createContext()
+export const SearchContext = createContext();
 
 export default function SongProvider(props) {
-	const [searchResults, setSearchResults] = useState()
-	const [currentSong, setCurrentSong] = useState("")
-	const [currentAlbum, setCurrentAlbum] = useState("")
-	const [currentArtist, setCurrentArtist] = useState("")
-	const[songDetail,setSongDetail] = useState("")
-	const[queueSong, setQueueSong] = useState("")
+  const [searchResults, setSearchResults] = useState();
+  const [currentSong, setCurrentSong] = useState("");
+  const [currentAlbum, setCurrentAlbum] = useState("");
+  const [currentArtist, setCurrentArtist] = useState("");
+  const [songDetail, setSongDetail] = useState("");
+  const [queueSong, setQueueSong] = useState("");
 
-	// ? next in queue view
-	// ? next from: playlist/album view
+  // ? next in queue view
+  // ? next from: playlist/album view
 
-	// ? next/ previous buttons
+  // ? next/ previous buttons
 
-	return (
-		<SearchContext.Provider
-			value={{
-				searchResults,
-				setSearchResults,
-				currentSong,
-				setCurrentSong,
-				currentAlbum, setCurrentAlbum,
-				currentArtist, setCurrentArtist,
-				songDetail,setSongDetail,
-				queueSong, setQueueSong
-			}}
-		>
-			{props.children}
-		</SearchContext.Provider>
-	)
+  return (
+    <SearchContext.Provider
+      value={{
+        searchResults,
+        setSearchResults,
+        currentSong,
+        setCurrentSong,
+        currentAlbum,
+        setCurrentAlbum,
+        currentArtist,
+        setCurrentArtist,
+        songDetail,
+        setSongDetail,
+        queueSong,
+        setQueueSong,
+      }}
+    >
+      {props.children}
+    </SearchContext.Provider>
+  );
 }
