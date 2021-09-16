0381b71dacf041897d8463117774a793c00e5204
chinduja-net <cindhujavijayaraj@gmail.com>
2021-09-16 15:21:19 +0200

    styles

--- Frontend/src/components/SearchBar.jsx
+++ Frontend/src/components/SearchBar.jsx
@@ -1,19 +1,7 @@
-import React, { useRef, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
-import { nanoid } from 'nanoid';
-
-import {
-  InputBase,
-  InputAdornment,
-  Grid,
-  Box,
-  ListItem,
-  Link,
-  Card,
-} from "@material-ui/core";
-import SearchIcon from "@material-ui/icons/Search";
-
-import { SearchContext } from '../context/SongProvider';
+import React, { useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { nanoid } from "nanoid";
+import { SearchContext } from "../context/SongProvider";
 
 function SearchBar() {
   const { setSearchResults } = useContext(SearchContext);
@@ -29,21 +17,21 @@ function SearchBar() {
     const response = await fetch(
       `https://yt-music-api.herokuapp.com/api/yt/search/${searchInput.current.value}`,
       {
-        method: 'GET',
+        method: "GET",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       }
     );
     const searchResult = await response.json();
-
+      console.log(searchInput.current.value)
     console.log(searchResult.content);
 
     let filteredResults = [];
 
     searchResult.content.forEach((obj) => {
       // Destructure the searchResult to get our desired properties we want to use
-      if (obj.type === 'song') {
+      if (obj.type === "song") {
         filteredResults.push({
           id: nanoid(),
           type: obj.type,
@@ -55,7 +43,7 @@ function SearchBar() {
           videoId: obj.videoId,
         });
       }
-      if (obj.type === 'album') {
+      if (obj.type === "album") {
         filteredResults.push({
           id: nanoid(),
           type: obj.type,
@@ -65,7 +53,7 @@ function SearchBar() {
           browseId: obj.browseId,
         });
       }
-      if (obj.type === 'artist') {
+      if (obj.type === "artist") {
         filteredResults.push({
           id: nanoid(),
           type: obj.type,
@@ -84,16 +72,14 @@ function SearchBar() {
     setSearchResults(filteredResults);
 
     // Redirect to the searchResults page if we are not on the home page
-    if (location.pathname === '/') {
-      history.push('/searchResults');
+    if (location.pathname === "/") {
+      history.push("/searchResults");
     }
   }
 
   return (
+
     <>
-    <Box display = "flex"
-    justifyContent = "center"
-    alignItems = "center">
       <form
         onSubmit={(e) => {
           // Todo: something
@@ -101,31 +87,17 @@ function SearchBar() {
           fetchApi();
         }}
       >
-        <InputBase
-          startAdornment = {
-            <InputAdornment position = "end">
-              <SearchIcon/>
-            </InputAdornment>
-          }
-          style={{
-            fontSize: 16,
-            background: "#E8EEF3",
-            borderRadius: 10,
-            textAlign: "center",
-            letterSpacing: -0.5,
-            width: 300,
-            marginTop: 30,
-          }}
-          type="text"
+        <input 
           // Saves a reference for the target element so that it can be used globally in the component
           ref={searchInput}
-          placeholder="Search for artists/album/songs"
+          placeholder="Search for artists/album/song"
         />
         
       </form>
-      </Box>
     </>
+    
   );
 }
 
 export default SearchBar;
+
--- Frontend/src/routes/SearchResults.jsx
+++ Frontend/src/routes/SearchResults.jsx
@@ -1,189 +1,187 @@
-import React, { useContext } from "react"
-import { useHistory } from "react-router-dom"
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
-import { Container } from "@material-ui/core"
+import { Grid, Container, Button, IconButton } from "@material-ui/core";
 
-import { SearchContext } from "../context/SongProvider"
-import SearchBar from "../components/SearchBar"
-import style from "../styles/SearchResults.module.css"
+import { SearchContext } from "../context/SongProvider";
+import SearchBar from "../components/SearchBar";
+//import style from "../styles/SearchResults.module.css"
 
 function SearchResults() {
-	const {
-		searchResults,
-		setCurrentSong,
-		setCurrentAlbum,
-		setCurrentArtist,
-		currentSong,
-		currentAlbum,
-		currentArtist,
-		setSongDetail,
-	} = useContext(SearchContext)
+  const {
+    searchResults,
+    setCurrentSong,
+    setCurrentAlbum,
+    setCurrentArtist,
+    currentSong,
+    currentAlbum,
+    currentArtist,
+    setSongDetail,
+  } = useContext(SearchContext);
 
-	const history = useHistory()
+  const history = useHistory();
 
-	function renderSong(object) {
-		return (
-			<Container
-				maxWidth="xs"
-				className="searchResultCard"
-				data-render-song={JSON.stringify(object)}
-				data-render-details={JSON.stringify(object)}
-				key={object.id}
-			>
-				<img
-					data-render-song={JSON.stringify(object)}
-					className={style.result_ThumbnailSquare}
-					src={object.thumbnails[0].url}
-					alt={object.artist + "'s cover thumbnail"}
-				/>
-				<div data-render-song={JSON.stringify(object)}>
-					<h4 data-render-song={JSON.stringify(object)}>{object.name}</h4>
-					<p data-render-song={JSON.stringify(object)}>{object.artist}</p>
-				</div>
-				<button type="click" onClick={showSongDetails}>
-					show Details
-				</button>
-			</Container>
-		)
-	}
+  function renderSong(object) {
+    return (
+      <Grid
+	 
+        data-render-song={JSON.stringify(object)}
+        data-render-details={JSON.stringify(object)}
+        key={object.id}
+      >
+        <img
+          data-render-song={JSON.stringify(object)}
+          src={object.thumbnails[0].url}
+          alt={object.artist + "'s cover thumbnail"}
+        />
+        {/* <div data-render-song={JSON.stringify(object)}> */}
+        <h4 data-render-song={JSON.stringify(object)}>{object.name}</h4>
+        {/* <p data-render-song={JSON.stringify(object)}>{object.artist}</p> */}
+        {/* </div> */}
+        <Button variant = "contained" type="click" onClick={showSongDetails}>
+          song details
+        </Button>
+      </Grid>
+    );
+  }
 
-	function renderArtist(object) {
-		return (
-			<Container
-				maxWidth="xs"
-				className="searchResultCard"
-				data-render-artist={JSON.stringify(object)}
-				key={object.id}
-			>
-				<p data-render-artist={JSON.stringify(object)}>{object.type}</p>
-				<img
-					data-render-artist={JSON.stringify(object)}
-					className={style.result_ThumbnailArtist}
-					src={object.thumbnails[0].url}
-					alt={object.name + "'s cover thumbnail"}
-				/>
-				<h3 data-render-artist={JSON.stringify(object)}>{object.name}</h3>
-			</Container>
-		)
-	}
-	function renderAlbum(object) {
-		return (
-			<Container
-				maxWidth="xs"
-				className="searchResultCard"
-				data-render-album={JSON.stringify(object)}
-				key={object.id}
-			>
-				<img
-					data-render-album={JSON.stringify(object)}
-					className={style.result_ThumbnailSquare}
-					src={object.thumbnails[0].url}
-					alt={object.artist + "'s cover thumbnail"}
-				/>
-				<div data-render-album={JSON.stringify(object)}>
-					<h4 data-render-album={JSON.stringify(object)}>{object.name}</h4>
-					<p data-render-album={JSON.stringify(object)}>{object.artist}</p>
-				</div>
-			</Container>
-		)
-	}
-	//Displays the details of the song in a new page or route
-	function showSongDetails(e) {
-		if (
-			e.currentTarget.parentElement.attributes.getNamedItem(
-				"data-render-details",
-			) !== null
-		) {
-			let clickedDetailSong = JSON.parse(
-				e.currentTarget.parentElement.attributes.getNamedItem(
-					"data-render-details",
-				).value,
-			)
-			console.log("parsed SONG_DETAILS: ", clickedDetailSong)
+  function renderArtist(object) {
+    return (
+      <Grid
+	 
+	 data-render-artist={JSON.stringify(object)} key={object.id}>
+        <p data-render-artist={JSON.stringify(object)}>{object.type}</p>
+        <img
+          data-render-artist={JSON.stringify(object)}
+          src={object.thumbnails[0].url}
+          alt={object.name + "'s cover thumbnail"}
+        />
+        <h3 data-render-artist={JSON.stringify(object)}>{object.name}</h3>
+      </Grid>
+    );
+  }
+  function renderAlbum(object) {
+    return (
+      <Grid 
+	   data-render-album={JSON.stringify(object)} key={object.id}>
+        <img
+          data-render-album={JSON.stringify(object)}
+          src={object.thumbnails[0].url}
+          alt={object.artist + "'s cover thumbnail"}
+        />
 
-			setSongDetail(clickedDetailSong)
+        <h4 data-render-album={JSON.stringify(object)}>{object.name}</h4>
+        <p data-render-album={JSON.stringify(object)}>{object.artist}</p>
+      </Grid>
+    );
+  }
+  //Displays the details of the song in a new page or route
+  function showSongDetails(e) {
+    if (
+      e.currentTarget.parentElement.attributes.getNamedItem(
+        "data-render-details"
+      ) !== null
+    ) {
+      let clickedDetailSong = JSON.parse(
+        e.currentTarget.parentElement.attributes.getNamedItem(
+          "data-render-details"
+        ).value
+      );
+      console.log("parsed SONG_DETAILS: ", clickedDetailSong);
 
-			history.push("/detailsPage")
-		}
-	}
+      setSongDetail(clickedDetailSong);
 
-	function resultsClickHandler(e) {
-		console.log("______________________")
+      history.push("/detailsPage");
+    }
+  }
 
-		/** This part could be used to find all the child nodes of an element in hopes of making a cleaner solution to the problem*/
-		// If the user clicks on a result card
-		// or any of the result card's child nodes
+  function resultsClickHandler(e) {
+    console.log("______________________");
 
-		// ! This solution may not work with material ui components since the IF statement searches for a DIV
+    /** This part could be used to find all the child nodes of an element in hopes of making a cleaner solution to the problem*/
+    // If the user clicks on a result card
+    // or any of the result card's child nodes
 
-		// ? Element.matches("selectorString") could be used to find parent element of e.target
+    // ! This solution may not work with material ui components since the IF statement searches for a DIV
 
-		// let addedChildNodes = []
-		// let directChildNodes = [...e.target.childNodes]
-		// directChildNodes.forEach((node) => {
-		// 	if (node.tagName === "DIV") addedChildNodes = [...node.childNodes]
-		// })
-		// directChildNodes = [...directChildNodes, ...addedChildNodes]
-		// console.log("COMPLETED CHILDNODES: ", directChildNodes)
+    // ? Element.matches("selectorString") could be used to find parent element of e.target
 
-		if (e.target.attributes.getNamedItem("data-render-song") !== null) {
-			let clickedValueSong = JSON.parse(
-				e.target.attributes.getNamedItem("data-render-song").value,
-			)
-			console.log("parsed SONG: ", clickedValueSong)
+    // let addedChildNodes = []
+    // let directChildNodes = [...e.target.childNodes]
+    // directChildNodes.forEach((node) => {
+    // 	if (node.tagName === "DIV") addedChildNodes = [...node.childNodes]
+    // })
+    // directChildNodes = [...directChildNodes, ...addedChildNodes]
+    // console.log("COMPLETED CHILDNODES: ", directChildNodes)
 
-			setCurrentSong(clickedValueSong)
+    if (e.target.attributes.getNamedItem("data-render-song") !== null) {
+      let clickedValueSong = JSON.parse(
+        e.target.attributes.getNamedItem("data-render-song").value
+      );
+      console.log("parsed SONG: ", clickedValueSong);
 
-			history.push("/playerPage")
-		}
+      setCurrentSong(clickedValueSong);
 
-		// Look at the clicked element and determine their types, then update the context with the element's connected data
-		// ARTIST
-		if (e.target.attributes.getNamedItem("data-render-artist") !== null) {
-			let clickedValueArtist = JSON.parse(
-				e.target.attributes.getNamedItem("data-render-artist").value,
-			)
-			console.log("parsed ARTIST: ", clickedValueArtist)
+      history.push("/playerPage");
+    }
 
-			setCurrentArtist(clickedValueArtist)
-		}
+    // Look at the clicked element and determine their types, then update the context with the element's connected data
+    // ARTIST
+    if (e.target.attributes.getNamedItem("data-render-artist") !== null) {
+      let clickedValueArtist = JSON.parse(
+        e.target.attributes.getNamedItem("data-render-artist").value
+      );
+      console.log("parsed ARTIST: ", clickedValueArtist);
 
-		// Look at the clicked element and determine their types, then update the context with the element's connected data
-		// ALBUM
-		if (e.target.attributes.getNamedItem("data-render-album") !== null) {
-			let clickedValueAlbum = JSON.parse(
-				e.target.attributes.getNamedItem("data-render-album").value,
-			)
-			console.log("parsed ALBUM: ", clickedValueAlbum)
+      setCurrentArtist(clickedValueArtist);
+    }
 
-			setCurrentAlbum(clickedValueAlbum)
-		}
+    // Look at the clicked element and determine their types, then update the context with the element's connected data
+    // ALBUM
+    if (e.target.attributes.getNamedItem("data-render-album") !== null) {
+      let clickedValueAlbum = JSON.parse(
+        e.target.attributes.getNamedItem("data-render-album").value
+      );
+      console.log("parsed ALBUM: ", clickedValueAlbum);
 
-		// ! Maybe a bug, the context doesn't seem to update instantly because it's always logging the PREVIOUS context value
-		console.log("currentSong Context: ", currentSong)
-		console.log("currentAlbum Context: ", currentAlbum)
-		console.log("currentArtist Context: ", currentArtist)
-	}
+      setCurrentAlbum(clickedValueAlbum);
+    }
 
-	return (
-		<div id="root">
-			<h5>SearchResults.jsx</h5>
-			<SearchBar />
-			<Container maxWidth="xs" onClick={resultsClickHandler}>
-				{searchResults // When looping through the search results we can limit the loop with user input
-					? searchResults.map((obj) => {
-							return obj.type === "song"
-								? renderSong(obj)
-								: obj.type === "artist"
-								? renderArtist(obj)
-								: obj.type === "album"
-								? renderAlbum(obj)
-								: null
-					  })
-					: null}
-			</Container>
-		</div>
-	)
+    // ! Maybe a bug, the context doesn't seem to update instantly because it's always logging the PREVIOUS context value
+    console.log("currentSong Context: ", currentSong);
+    console.log("currentAlbum Context: ", currentAlbum);
+    console.log("currentArtist Context: ", currentArtist);
+  }
+
+  return (
+    <div id="root">
+      <h5>SearchResults</h5>
+      <SearchBar />
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        maxwidth="xs"
+        style={{
+          background: "#FFEED3",
+        }}
+        onClick={resultsClickHandler}
+      >
+        {searchResults // When looping through the search results we can limit the loop with user input
+          ? searchResults.map((obj) => {
+              return obj.type === "song"
+                ? renderSong(obj)
+                : obj.type === "artist"
+                ? renderArtist(obj)
+                : obj.type === "album"
+                ? renderAlbum(obj)
+                : null;
+            })
+          : null}
+      </Grid>
+    </div>
+  );
 }
 
-export default SearchResults
+export default SearchResults;
