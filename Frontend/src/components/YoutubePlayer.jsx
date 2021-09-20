f97d376082ed719d6e93c0708fcaea276adf9a76
paccao <16489683+paccao@users.noreply.github.com>
2021-09-20 16:25:23 +0200

    handle state changes in the yt api and update context accordingly

--- Frontend/src/components/YoutubePlayer.jsx
+++ Frontend/src/components/YoutubePlayer.jsx
@@ -4,8 +4,14 @@ import YouTube from "react-youtube"
 import { SearchContext } from "../context/SongProvider"
 
 function YoutubePlayer() {
-	const { currentSong, queueSongs, shiftQueue, setCurrentSong } =
-		useContext(SearchContext)
+	const {
+		currentSong,
+		queueSongs,
+		shiftQueue,
+		setCurrentSong,
+		ytPlayerState,
+		setYtPlayerState,
+	} = useContext(SearchContext)
 
 	const opts = {
 		height: "390",
@@ -25,15 +31,56 @@ function YoutubePlayer() {
 		}
 	}
 
+	// Call this on mount
 	function _onReady(event) {
-		// access to player in all event handlers via event.target
+		// Autoplay player on mount
 		event.target.playVideo()
 	}
 
+	// Autoplay next song in queue if it exists
 	function _onEnd(event) {
-		// access to player in all event handlers via event.target
-		console.log(event.target.i.h.videoId)
 		queueSongs ? playNextSong(event) : null
+	}
+
+	// Calls 2 functions depending on if playingState is true / false.
+	// Toggles between playing and pauseing player
+	function toggleVidBtn(event) {
+		const e = event.target
+		console.log("player state", ytPlayerState)
+		ytPlayerState === 1
+			? e.pauseVideo()
+			: ytPlayerState === 2
+			? e.playVideo()
+			: null
+	}
+
+	function _onStateChange(event) {
+		let state = event.target.getPlayerState()
+		switch (state) {
+			case -1:
+				setYtPlayerState(-1) // unstarted
+				break
+			case 0:
+				setYtPlayerState(0) // ended
+				break
+			case 1:
+				setYtPlayerState(1) // playing
+				break
+			case 2:
+				setYtPlayerState(2) // paused
+				break
+			case 3:
+				setYtPlayerState(3) // buffering
+				break
+			case 5:
+				setYtPlayerState(5) // video cued
+				break
+			default:
+				return console.log("Unknown yt player state.")
+		}
+
+		console.log("CURRENT STATE:", ytPlayerState)
+		toggleVidBtn(event)
 	}
 
 	return currentSong ? (
@@ -42,6 +89,7 @@ function YoutubePlayer() {
 			opts={opts}
 			onReady={_onReady}
 			onEnd={_onEnd}
+			onStateChange={_onStateChange}
 		/>
 	) : null
 }
--- Frontend/src/context/SongProvider.jsx
+++ Frontend/src/context/SongProvider.jsx
@@ -1,49 +1,52 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from "react"
 
-export const SearchContext = createContext();
+export const SearchContext = createContext()
 
 export default function SongProvider(props) {
-  const [searchResults, setSearchResults] = useState();
-  const [currentSong, setCurrentSong] = useState();
-  const [currentAlbum, setCurrentAlbum] = useState();
-  const [currentArtist, setCurrentArtist] = useState();
-  const [songDetail, setSongDetail] = useState({});
-  const [queueSongs, setQueueSongs] = useState([]);
+	const [searchResults, setSearchResults] = useState()
+	const [currentSong, setCurrentSong] = useState()
+	const [currentAlbum, setCurrentAlbum] = useState()
+	const [currentArtist, setCurrentArtist] = useState()
+	const [songDetail, setSongDetail] = useState({})
+	const [queueSongs, setQueueSongs] = useState([])
+	const [ytPlayerState, setYtPlayerState] = useState(-1) // -1 state is unstarted in youtube's api
 
-  // ? next in queue view
-  // ? next from: playlist/album view
+	// ? next in queue view
+	// ? next from: playlist/album view
 
-  // ? next/ previous buttons
+	// ? next/ previous buttons
 
-  function addObjToArray(newObj) {
-    setQueueSongs([...queueSongs, newObj]);
-  }
+	function addObjToArray(newObj) {
+		setQueueSongs([...queueSongs, newObj])
+	}
 
-  function shiftQueue() {
-    const newArr = queueSongs;
-    newArr.shift();
-    setQueueSongs(newArr);
-  }
+	function shiftQueue() {
+		const newArr = queueSongs
+		newArr.shift()
+		setQueueSongs(newArr)
+	}
 
-  return (
-    <SearchContext.Provider
-      value={{
-        searchResults,
-        setSearchResults,
-        currentSong,
-        setCurrentSong,
-        currentAlbum,
-        setCurrentAlbum,
-        currentArtist,
-        setCurrentArtist,
-        songDetail,
-        setSongDetail,
-        queueSongs,
-        addObjToArray,
-        shiftQueue,
-      }}
-    >
-      {props.children}
-    </SearchContext.Provider>
-  );
+	return (
+		<SearchContext.Provider
+			value={{
+				searchResults,
+				setSearchResults,
+				currentSong,
+				setCurrentSong,
+				currentAlbum,
+				setCurrentAlbum,
+				currentArtist,
+				setCurrentArtist,
+				songDetail,
+				setSongDetail,
+				queueSongs,
+				addObjToArray,
+				shiftQueue,
+				ytPlayerState,
+				setYtPlayerState,
+			}}
+		>
+			{props.children}
+		</SearchContext.Provider>
+	)
 }
