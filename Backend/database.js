2b5a7c71dce6d0f9f7241a021be475d19230a511
chinduja-net <cindhujavijayaraj@gmail.com>
2021-09-21 14:11:28 +0200

    backend signup in progress

--- Backend/database.js
+++ Backend/database.js
@@ -2,9 +2,18 @@ const sqlite = require('better-sqlite3');
 
 const conn = sqlite('database.db');
 
+const {nanoid} = require('nanoid');
+const jwt = require('jsonwebtoken');
+
+
 function all(query, params = {}) {
   const stmt = conn.prepare(query);
   return stmt.all(params);
+}
+function run(query, params = {}) {
+  // prepare statement
+  const stmt = conn.prepare(query)
+  return stmt.run(params)
 }
 
 module.exports = {
@@ -12,4 +21,14 @@ module.exports = {
     let users = all(`SELECT userName, password FROM users`);
     return users;
   },
+
+  createAccount(account){
+
+    const query = `INSERT INTO users(userName, password) VALUES(:userName,:password)`
+    return run(query, account)
+
+    
+  }
 };
+
+ 
