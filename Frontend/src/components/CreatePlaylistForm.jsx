75679de07c4d54de333afb69a0758da8840461a3
chinduja-net <cindhujavijayaraj@gmail.com>
2021-09-27 12:00:58 +0200

    testing

--- Frontend/src/components/CreatePlaylistForm.jsx
+++ Frontend/src/components/CreatePlaylistForm.jsx
@@ -1,37 +1,35 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
 import { useHistory } from "react-router";
 import { isLoggedIn } from "../services/authService";
 
 
 function CreatePlaylistForm() {
-    const [playlistName, setPlaylistName] = useState();
-    const history = useHistory();
+   
+ const [playlistName, setPlaylistName] = useState();
+  const history = useHistory();
 
-    function setName(e){
-        setPlaylistName(e.target.value)
-        history.push('/')
+  function setName(e) {
+    setPlaylistName(e.target.value);
+    
+  }
 
-    }
-
-    return (
+  return (
+    <div>
+      {isLoggedIn() ? (
+        <form action="submit">
+          <input name="playlistname" type="text" onChange={setName} />
+          <label htmlFor="playlistname">Give your playlist a name</label>
+        </form>
+      ) : (
         <div>
-            {
-                isLoggedIn() ? (
-                <form action="submit">
-                <input
-                  name="playlistname"
-                  type="text"
-                  onChange={setName}
-                />
-                <label htmlFor="playlistname">Give your playlist a name</label>
-              </form>) : (<div><p>you need to login first ! <a href="/">click here</a></p></div>)
-
-
-            }
+          <p>
+            you need to login first ! <a href="/">click here</a>
+          </p>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default CreatePlaylistForm
-
+export default CreatePlaylistForm;
